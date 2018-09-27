4bc80d096097a80af6a15d514a80091eb3e7bf18
Farstarz <vaibhav.pandey9890@gmail.com>
2018-09-26 23:58:23 -0700

    final update

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
   // question index
   var index= 0;
+  var currentIndex;
+  $(".nextQuestion").html("<h2>Next Question</h2>");
+  $(".showResults").html("<h2>Show Results</h2>");
+  $(".reset").html("<h2>Reset Quiz</h2>");
+                    
   var trivia = {
+    //score
+    score: 0,
     // store questions in an array
     questions: ["Q.1 Jack Sparrow...*ahem*...Captain Jack Sparrow is first seen entering Port Royal in POTC1, on a small sinking boat. Who did he steal trivia boat from?",
                 "Q.2 Where is Will Turner first seen in the first POTC?",
@@ -22,55 +29,133 @@ $(document).ready(function(){
     answers5: ["Lord Cutler Beckett","Drinking with Gibbs at a bar.","Ragetti","Klaus Badelt","A ride at Disneyland.","Port Royal","6","Captain Jack","Paul Sternyard","Captain Jack Sparrow"],
     // user choice array
     userChoice: [],
+    //answers array
+    ansOption: ["Anna-Maria", "Floating on a platform of wood in the sea.", "Bo'sun", "Pirate's Life", "A video game.", "Port Coleen", "5", "Pintel", "Mackenzie Crook", "Commodore Norrington"],
     // display question
     updateQuestion: function(){
-      console.log("updateQuestion Ran:");
-      $("#questions").html("<h2>"+trivia.questions[index]+"</h2>");
-      $("#answers").html("<input type=\"radio\" name=\"question"+index+"\" value=\"1\">"+trivia.answers1[index]+"</input><input type=\"radio\" name=\"question"+index+"\" value=\"2\">"+trivia.answers2[index]+"</input><input type=\"radio\" name=\"question"+index+"\" value=\"3\">"+trivia.answers3[index]+"</input><input type=\"radio\" name=\"question"+index+"\" value=\"4\">"+trivia.answers4[index]+"</input><input type=\"radio\" name=\"question"+index+"\" value=\"5\">"+trivia.answers5[index]+"</input>");
-      index = index +1;
-      trivia.nextQuestion();
+      // console.log("updateQuestion Ran:");
+      // console.log(index);
+      $(".question").html("<h2>"+trivia.questions[index]+"</h2>");
+      $(".answer1").html("<input type=\"radio\" name=\"question"+index+"\" value="+trivia.answers1[index]+">"+trivia.answers1[index]+"</input>");
+      $(".answer2").html("<input type=\"radio\" name=\"question"+index+"\" value="+trivia.answers2[index]+">"+trivia.answers2[index]+"</input>");
+      $(".answer3").html("<input type=\"radio\" name=\"question"+index+"\" value="+trivia.answers3[index]+">"+trivia.answers3[index]+"</input>");
+      $(".answer4").html("<input type=\"radio\" name=\"question"+index+"\" value="+trivia.answers4[index]+">"+trivia.answers4[index]+"</input>");
+      $(".answer5").html("<input type=\"radio\" name=\"question"+index+"\" value="+trivia.answers5[index]+">"+trivia.answers5[index]+"</input>");
+      currentIndex = index;
+      index++;
+      // trivia.nextQuestion();
           
       },
     // store answer to a variable
     getChoice: function(){
-      trivia.userChoice[index-1] = $("input[name=\"question"+(index-1)+"\"]:checked").val();
-      console.log("getChoice ran");
+      trivia.userChoice[currentIndex] = $("input[name=\"question"+(currentIndex)+"\"]:checked").val();
+      // console.log("getChoice ran");
+        
     },
     // next question
     nextQuestion: function(){
-      console.log("nextQuestion Ran:");
+      // console.log("nextQuestion Ran:");
       if(index<9){
-        $(".command").html("<h2 id=\"NextQ\">"+"Next Question</h2>").on("click", function(){
-          trivia.getChoice();
-          trivia.updateQuestion();
-        });  
-      }else{
-        $(".command").html("<h2 id=\"ShowR\">"+"Show Result</h2>").on("click", function(){
-          console.log(index);
-          trivia.showResult();
-        });
-      };
+        $(".nextQuestion").show("slow");
+        $(".showResults").hide("slow");
+        $(".reset").hide("slow");
+        
+        // .on("click", function(){
+          // trivia.getChoice();
+          // trivia.updateQuestion();
+          
+      }else if(index===10){
+            // $("#showR").html("<h2>Show Result</h2>").on("click", function(){
+            // $("#nextQ").hide();
+            // $("#showR").show();
+            // $("#resetQ").hide();
+            // console.log(index);
+              // trivia.showResult();
+            // });
+          }else{
+            $(".question").empty();
+            $(".answer1").empty();
+            $(".answer2").empty();
+            $(".answer3").empty();
+            $(".answer4").empty();
+            $(".answer5").empty();
+
+          };
     },
     // show result
     showResult: function(){
-      $("#questions").empty();
-      trivia.userChoice.forEach(element => {
-        $("#questions").append("<h3>Option "+element+"</h3>");  
-      });
-      $("#answers").empty();
-      $(".command").html("<h2 id=\"ResetQ\">"+"Reset Quiz").on("click",function(){
-        trivia.reset();
-      });
+      currentIndex = index;
+      
+      $(".question").empty();
+      $(".answer1").empty();
+      $(".answer2").empty();
+      $(".answer3").empty();
+      $(".answer4").empty();
+      $(".answer5").empty();
+      
+      for(var i =0; i<trivia.userChoice.length;i++){
+          
+        if(trivia.userChoice[i]===trivia.ansOption[i]){
+          this.score++;
+          $(".question").append("<h3>Answer "+(i+1)+": "+trivia.ansOption[i]+", your answer is correct!");
+        }else{
+          $(".question").append("<h3>Answer "+(i+1)+": "+trivia.ansOption[i]+", you answered: "+trivia.userChoice[i]+", your answer is wrong!");
+        }
+      };
+      $(".answer3").html("<h2>You got "+this.score+"answers correct!");
+      // $("#nextQ").show();
+      // $("#showR").hide();
+      // $("#resetQ").hide();
+      // .on("click",function(){
+      //   trivia.reset();
+      // });
       // $("#questions").html("<h3>Option "+trivia.userChoice+"</h3>");
 
     },
     reset: function(){
-      console.log("reset ran");
+      // console.log("reset ran");
       index = 0;
       trivia.userChoice=[];
       trivia.updateQuestion();
     } 
   };
   trivia.updateQuestion();
+  $(".nextQuestion").on("click", function(){
+    if(index<10){
+      trivia.getChoice();
+      trivia.updateQuestion();
+    }
+  });
+  $(".showResults").on("click", function(){
+    trivia.getChoice();
+    trivia.showResult();
+  });
+  $(".reset").on("click", function(){
+    trivia.reset();
+  });
+
+  // animate sprites
+  var tID;
+    
+  function animateScript(){
+    var positionX= 0;
+    var positionY=0;
+    var interval=500;
+  //create sprite animation function
+    tID = setInterval(()=>{
+      $("#sprite1").css("background-position", `-${positionX}px ${positionY}px`);
+
+      if(positionX < 300){
+        positionX+=60;
+      }else{
+        positionX=0;
+        positionY+=90;
+      }
+      if(positionY>180){
+        positionY = 0;
+      }
+    }, interval);
+  }
+  // animateScript();
 });
 
